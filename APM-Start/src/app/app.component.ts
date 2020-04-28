03b2e3ba022f00599901f7ff67ea3dc392c5ac9f
Ryan Freeman <rfreeman@soleran.com>
2020-04-28 17:21:48 -0500

    in chapter 7

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'ACME Product Management';
 }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
